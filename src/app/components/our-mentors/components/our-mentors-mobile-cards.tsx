4e2b3900f01c16071c8d359d156c10580790dfa2
Rojkov Yury <rojkov.yury@gmail.com>
2025-04-12 15:00:29 +0300

    OurMentorMobileCards refactor

--- src/app/components/our-mentors/components/our-mentors-mobile-cards.tsx
+++ src/app/components/our-mentors/components/our-mentors-mobile-cards.tsx
@@ -7,10 +7,8 @@ import mentor2 from '../../../../../public/images/our-mentors-2.png'
 import mentor3 from '../../../../../public/images/our-mentors-3.png'
 import OurMentorMobileCard from "./our-mentors-mobile-card";
 
-
 export default function OurMentorMobileCards() {
-  const [currentCardSelected, setIsLastOneSelected] = useState(0);
-  const [currentCardScroll, setCurrentCardScroll] = useState(1);
+  const [currentCardSelected, setIsLastOneSelected] = useState(1);
   const cardRefMentor1 = useRef(null);
   const cardRefMentor2 = useRef(null);
   const cardRefMentor3 = useRef(null);
@@ -21,9 +19,9 @@ export default function OurMentorMobileCards() {
         behavior: 'smooth',
         // block: 'center',
         block: 'nearest',
-        inline: currentCardSelected === 1
+        inline: currentCardSelected == 1
           ? 'start'
-          : currentCardSelected === 2
+          : currentCardSelected == 2
             ? 'center'
             : 'end',
       });
@@ -31,17 +29,14 @@ export default function OurMentorMobileCards() {
   };
 
   useEffect(() => {
-    if (currentCardSelected === 1) {
+    if (currentCardSelected == 1) {
       scrollToCard(cardRefMentor1);
-      setCurrentCardScroll(1);
     }
-    if (currentCardSelected === 2) {
+    if (currentCardSelected == 2) {
       scrollToCard(cardRefMentor2);
-      setCurrentCardScroll(2);
     }
-    if (currentCardSelected === 3) {
+    if (currentCardSelected == 3) {
       scrollToCard(cardRefMentor3);
-      setCurrentCardScroll(3);
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentCardSelected]);
@@ -95,50 +90,50 @@ export default function OurMentorMobileCards() {
         <Box
           onClick={() => setIsLastOneSelected(1)}
           sx={{
+            width: currentCardSelected == 1 ? '75%' : '25%',
+            transformOrigin: 'left',
             display: 'flex',
             alignItems: 'center',
-            width: currentCardScroll === 1 ? '75%' : '25%',
-            transformOrigin: 'left',
             ml: '16px',
             mr: '8px',
             height: '40px',
             cursor: 'pointer',
-            transition: 'width 0.2s ease',
+            transition: 'width 0.4s ease',
           }}
         >
-          <Box sx={{ width: '100%', height: '4px', borderRadius: '2px', bgcolor: currentCardScroll === 1 ? '#FFA700' : '#C9D3E8' }} />
+          <Box sx={{ width: '100%', height: '4px', borderRadius: '2px', bgcolor: currentCardSelected == 1 ? '#FFA700' : '#C9D3E8' }} />
         </Box>
 
         <Box
           onClick={() => setIsLastOneSelected(2)}
           sx={{
+            width: currentCardSelected == 2 ? '75%' : '25%',
             display: 'flex',
             alignItems: 'center',
-            width: currentCardScroll === 2 ? '75%' : '25%',
             mx: '8px',
             height: '40px',
             cursor: 'pointer',
-            transition: 'width 0.2s ease',
+            transition: 'width 0.4s ease',
           }}
         >
-          <Box sx={{ width: '100%', height: '4px', borderRadius: '2px', bgcolor: currentCardScroll === 2 ? '#FFA700' : '#C9D3E8' }} />
+          <Box sx={{ width: '100%', height: '4px', borderRadius: '2px', bgcolor: currentCardSelected == 2 ? '#FFA700' : '#C9D3E8' }} />
         </Box>
 
         <Box
           onClick={() => setIsLastOneSelected(3)}
           sx={{
+            width: currentCardSelected == 3 ? '75%' : '25%',
+            transformOrigin: 'right',
+            ml: '8px',
             display: 'flex',
             alignItems: 'center',
-            width: currentCardScroll === 3 ? '75%' : '25%',
-            transformOrigin: 'right',
             mr: '16px',
-            ml: '8px',
             height: '40px',
             cursor: 'pointer',
-            transition: 'width 0.2s ease',
+            transition: 'width 0.4s ease',
           }}
         >
-          <Box sx={{ width: '100%', height: '4px', borderRadius: '2px', bgcolor: currentCardScroll === 3 ? '#FFA700' : '#C9D3E8' }} />
+          <Box sx={{ width: '100%', height: '4px', borderRadius: '2px', bgcolor: currentCardSelected == 3 ? '#FFA700' : '#C9D3E8' }} />
         </Box>
       </Box>
     </Box>
