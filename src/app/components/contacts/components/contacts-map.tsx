0a2f413e81bab071c5d8db409e8e95816bb8f24c
Rojkov Yury <rojkov.yury@gmail.com>
2025-04-14 01:11:35 +0300

    ContactsMap

--- src/app/components/contacts/components/contacts-map.tsx
+++ src/app/components/contacts/components/contacts-map.tsx
@@ -15,8 +15,40 @@ export default async function ContactsMap() {
           mb: { xs: '32px', sm: '32px', md: '0px', lg: '0px', xl: '0px' },
           borderRadius: '8px',
           mx: { xs: '16px', sm: '16px', md: '20px', lg: '20px', xl: '0px' },
+          '& .popup._type_normal._position_top': {
+            bgcolor: 'red',
+          },
         }}
       >
+        <Box sx={{ width: '100%', height: '100%', display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' } }}>
+          <iframe
+            src="https://yandex.ru/map-widget/v1/?ll=37.581067%2C54.192753&mode=search&whatshere%5Bpoint%5D=37.585811%2C54.192142&whatshere%5Bzoom%5D=17&z=16"
+            width="100%"
+            height="100%"
+            allowFullScreen={true}
+            style={{
+              position: 'relative',
+              border: 'none',
+              borderRadius: '8px',
+              overflow: 'hidden',
+            }}
+          />
+        </Box>
+        <Box sx={{ width: '100%', height: '100%', display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', xl: 'none' } }}>
+          <iframe
+            src="https://yandex.ru/map-widget/v1/?ll=37.585667%2C54.192753&mode=search&whatshere%5Bpoint%5D=37.585811%2C54.192142&whatshere%5Bzoom%5D=17&z=16"
+            width="100%"
+            height="100%"
+            allowFullScreen={true}
+            style={{
+              position: 'relative',
+              border: 'none',
+              borderRadius: '8px',
+              overflow: 'hidden',
+            }}
+          />
+        </Box>
+
         <Box
           sx={{
             display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' },
@@ -29,13 +61,14 @@ export default async function ContactsMap() {
         </Box>
       </Box>
 
+      {/* Mobile */}
       <Box
         sx={{
           display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', xl: 'none' },
           mx: 'auto',
         }}
       >
-        <SignUpContainer text="Обратный звонок" sx={{ boxShadow: '0px 12px 20px 0px #13264D1F' }}/>
+        <SignUpContainer text="Обратный звонок" sx={{ boxShadow: '0px 12px 20px 0px #13264D1F' }} />
       </Box>
     </>
   )
