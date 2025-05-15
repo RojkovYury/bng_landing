eb30de65b1d31f46f3ec0fb7ad5351c9635ef71b
Rojkov Yury <rojkov.yury@gmail.com>
2025-05-15 23:59:21 +0300

    TrainingCenterProgramBg

--- src/app/components/training-center-program/components/training-center-program-bg.tsx
+++ src/app/components/training-center-program/components/training-center-program-bg.tsx
@@ -3,38 +3,21 @@
 import { Box } from "@mui/material";
 import Image from 'next/image';
 import bg from '../../../../../public/images/training-center-program-bg.png';
-import bgm from '../../../../../public/images/training-center-program-bg-mobile.png';
 
 export default function TrainingCenterProgramBg() {
   return (
-    <>
-      <Box
-        sx={{
-          position: 'absolute',
-          zIndex: 2,
-          top: '0px',
-          left: '0px',
-          display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' },
-          width: '836px',
-          height: '572px',
-        }}
-      >
-        <Image src={bg} alt="" fill/>
-      </Box>
-
-      <Box
-        sx={{
-          position: 'absolute',
-          zIndex: 2,
-          top: '0px',
-          left: '0px',
-          display: { xs: 'flex', sm: 'flex', md: 'none', lg: 'none', xl: 'none' },
-          width: '343px',
-          height: '358px',
-        }}
-      >
-        <Image src={bgm} alt="" fill/>
-      </Box>
-    </>
+    <Box
+      sx={{
+        position: 'absolute',
+        zIndex: 2,
+        top: { xs: '-30px', sm: '-30px', md: '-50px', lg: '-50px', xl: '-50px' },
+        left: '0px',
+        display: 'flex',
+        width: { xs: '500px', sm: '500px', md: '836px', lg: '836px', xl: '836px' },
+        height: { xs: '500px', sm: '500px', md: '836px', lg: '836px', xl: '836px' },
+      }}
+    >
+      <Image src={bg} alt="" fill/>
+    </Box>
   )
 }
