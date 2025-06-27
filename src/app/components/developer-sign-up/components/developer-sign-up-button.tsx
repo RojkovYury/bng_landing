55a892ab16210f2613a41f552bfeb313e2ede536
Rojkov Yury <rojkov.yury@gmail.com>
2025-06-27 03:40:55 +0300

    in DeveloperSignUpButton ADD SocialNetworkIcons

--- src/app/components/developer-sign-up/components/developer-sign-up-button.tsx
+++ src/app/components/developer-sign-up/components/developer-sign-up-button.tsx
@@ -1,38 +1,49 @@
 'use client';
 
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useState } from "react";
 import SingUpModal from "../../../../components/sign-up-modal";
 import SingUpDrawer from "../../../../components/sign-up-drawer";
+import SocialNetworkIcons from "@/components/social-network-icons";
 
 export default function DeveloperSignUpButton() {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   return (
     <>
-      <Button
-        onClick={handleOpen}
-        variant="contained"
+      <Box
         sx={{
-          borderRadius: '8px',
-          width: { xs: '160px', sm: '160px', md: '180px', lg: '180px', xl: '180px' },
-          height: { xs: '50px', sm: '50px', md: '56px', lg: '56px', xl: '56px' },
-          bgcolor: '#1144AA',
-          boxShadow: 'none',
+          display: 'flex',
+          alignItems: 'center',
           mb: { xs: '80px', sm: '80px', md: '80px', lg: '160px', xl: '160px' },
-
-          textTransform: 'none',
-          fontSize: { xs: '18px', sm: '18px', md: '20px', lg: '20px', xl: '20px' },
-          fontWeight: 700, 
-          lineHeight: { xs: '22px', sm: '22px', md: '24px', lg: '24px', xl: '24px' },
-          '&:hover': {
-            bgcolor: '#184AAC',
-            boxShadow: 'none',
-          },
         }}
       >
-        Записаться
-      </Button>
+        <Button
+          onClick={handleOpen}
+          variant="contained"
+          sx={{
+            borderRadius: '8px',
+            width: { xs: '160px', sm: '160px', md: '180px', lg: '180px', xl: '180px' },
+            height: { xs: '50px', sm: '50px', md: '56px', lg: '56px', xl: '56px' },
+            bgcolor: '#1144AA',
+            boxShadow: 'none',
+
+            textTransform: 'none',
+            fontSize: { xs: '18px', sm: '18px', md: '20px', lg: '20px', xl: '20px' },
+            fontWeight: 700, 
+            lineHeight: { xs: '22px', sm: '22px', md: '24px', lg: '24px', xl: '24px' },
+            '&:hover': {
+              bgcolor: '#184AAC',
+              boxShadow: 'none',
+            },
+          }}
+        >
+          Записаться
+        </Button>
+        <Box sx={{ ml: '8px' }}>
+          <SocialNetworkIcons large />
+        </Box>
+      </Box>
 
       <SingUpModal open={open} setOpen={setOpen} rootSx={{ display: { xs: 'none', sm: 'flex', md: 'flex', lg: 'flex', xl: 'flex' } }} />
       <SingUpDrawer open={open} setOpen={setOpen} rootSx={{ display: { xs: 'flex', sm: 'none', md: 'none', lg: 'none', xl: 'none' } }}/>
--- src/components/social-network-icons.tsx
+++ src/components/social-network-icons.tsx
@@ -5,22 +5,29 @@ import Image from 'next/image';
 import tg from '../../public/icons/tg.png'
 import wa from '../../public/icons/wa.png'
 
-const iconButtonStyle = {
-  width: '22px',
-  height: '22px',
-  transition: 'all 0.3s',
-  "&:hover": {
-    scale: '1.2',
-  }
+interface SocialNetworkIconsProps {
+  large?: boolean;
 }
 
-export default function SocialNetworkIcons() {
+export default function SocialNetworkIcons({ large }: SocialNetworkIconsProps) {
   return (
     <Box>
       <IconButton
         href='https://t.me/academybng'
         target='_blank'
-        sx={{mr: '8px', ...iconButtonStyle}}
+        sx={{
+          mr: '8px',
+          width: large
+            ? { xs: '36px', sm: '36px', md: '46px', lg: '46px', xl: '46px' }
+            : '22px',
+          height: large
+            ? { xs: '36px', sm: '36px', md: '46px', lg: '46px', xl: '46px' }
+            : '22px',
+          transition: 'all 0.3s',
+          "&:hover": {
+            scale: '1.2',
+          }
+        }}
       >
         <Image src={tg} alt="tg" fill priority={true}/>
       </IconButton>
@@ -28,7 +35,18 @@ export default function SocialNetworkIcons() {
       <IconButton
        href='https://wa.me/+79330283525'
         target='_blank'
-        sx={{ ...iconButtonStyle }}
+        sx={{
+          width: large
+            ? { xs: '36px', sm: '36px', md: '46px', lg: '46px', xl: '46px' }
+            : '22px',
+          height: large
+            ? { xs: '36px', sm: '36px', md: '46px', lg: '46px', xl: '46px' }
+            : '22px',
+          transition: 'all 0.3s',
+          "&:hover": {
+            scale: '1.2',
+          }
+        }}
       >
         <Image src={wa} alt="wa" fill priority={true}/>
       </IconButton>
